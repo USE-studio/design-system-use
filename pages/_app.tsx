54712f20055782e513f7e36092afe42136c09388
Niels Christian Konrad Bisgaard <mail@nckn.dk>
2022-08-30 14:57:00 +0200

    Starting to follow this tutorial Learn Next.js With TypeScript in 30 Minutes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
+import './api'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
